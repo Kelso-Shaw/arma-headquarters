51fd526d699241157ee885276c88f27f26e9f705
Kelso <kelsey.shaw@icabbi.com>
2024-06-07 00:35:19 +0100

    Added: Default admin seeder

---
+++ api/seeders/20240606231653-admin-user.js
@@ -0,0 +1,25 @@
+"use strict";
+const bcrypt = require("bcrypt");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const hashedPassword = await bcrypt.hash("password123", 10);
+    return queryInterface.bulkInsert("Users", [
+      {
+        username: "Administrator",
+        email: "admin@" + process.env.HOST,
+        password: hashedPassword,
+        name: "Administrator",
+        role: "admin",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
+  },
+};
