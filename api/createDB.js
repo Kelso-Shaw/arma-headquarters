47f651c7912441e2c620c026848cbe2a74efabab
Kelso <kelsey.shaw@icabbi.com>
2024-06-06 15:56:46 +0100

    Updated install scripts

--- api/createDB.js
+++ api/createDB.js
@@ -1,5 +1,5 @@
-require('dotenv').config();  // This loads the environment variables from the .env file
-const { Sequelize } = require('sequelize');
+require("dotenv").config({ path: "../.env" });
+const { Sequelize } = require("sequelize");
 
 // Database configuration from environment variables
 const databaseName = process.env.DB_NAME;
@@ -9,14 +9,14 @@ const host = process.env.DB_HOST;
 
 // Create a connection to the database management system (DBMS), not the specific database
 const sequelize = new Sequelize(`mysql://${username}:${password}@${host}/`, {
-  logging: false // Turn off logging
+  logging: false, // Turn off logging
 });
 
 async function initializeDatabase() {
   try {
     // Check if the database exists by trying to connect to it
     const tempSequelize = new Sequelize(databaseName, username, password, {
-      dialect: 'mysql',
+      dialect: "mysql",
       host,
     });
     await tempSequelize.authenticate();
